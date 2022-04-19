7832352bb21b5634a4f99e78e37c485729c90f7b
Khakim <khakim.zhumagaliyev@mail.ru>
2022-04-19 19:01:37 +0600

    added OOP

--- dijkstra.py
+++ dijkstra.py
@@ -1,56 +1,57 @@
 from collections import defaultdict
 
-def build_graph(edge_list):
-    graph = defaultdict(list)
-    seen_edges = defaultdict(int)
-    for src, dst, weight in edge_list:
-        seen_edges[(src, dst, weight)] += 1
-        if seen_edges[(src, dst, weight)] > 1:  # checking for duplicated edge entries
-            continue
-        graph[src].append((dst, weight))
-        graph[dst].append((src, weight))  # remove this line of edge list is directed
-    return graph
 
+class Graph:
+    def __init__(self, edge_list):
+        self.graph = defaultdict(list)
+        self.edge_list = edge_list
 
-def dijkstra(graph, src, dst=None):
-    nodes = []
-    for n in graph:
-        nodes.append(n)
-        nodes += [x[0] for x in graph[n]]
+    def build_graph(self):
+        seen_edges = defaultdict(int)
+        for src, dst, weight in self.edge_list:
+            seen_edges[(src, dst, weight)] += 1
+            if seen_edges[(src, dst, weight)] > 1:  # checking for duplicated edge entries
+                continue
+            self.graph[src].append((dst, weight))
+            self.graph[dst].append((src, weight))  # remove this line of edge list is directed
+        return self.graph
 
-    q = set(nodes)
-    nodes = list(q)
-    dist = dict()
-    prev = dict()
-    for n in nodes:
-        dist[n] = float('inf')
-        prev[n] = None
+    def dijkstra(self, src, dst=None):
+        nodes = []
+        for n in self.graph:
+            nodes.append(n)
 
-    dist[src] = 0
+        q = set(nodes)
+        nodes = list(q)
+        dist = dict()   # distance to start
+        prev = dict()   # previous node in shortest path
+        for n in nodes:
+            dist[n] = float('inf')
+            prev[n] = None
 
-    while q:
-        u = min(q, key=dist.get)
-        q.remove(u)
+        dist[src] = 0
 
-        if dst is not None and u == dst:
-            return dist[dst], prev
+        while q:
+            u = min(q, key=dist.get)
+            q.remove(u)
 
-        for v, w in graph.get(u, ()):
-            alt = dist[u] + w
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
+            if dst is not None and u == dst:
+                return dist[dst], prev
 
-    return dist, prev
+            for v, w in self.graph.get(u, ()):
+                newdist = dist[u] + w
+                if newdist < dist[v]:
+                    dist[v] = newdist
+                    prev[v] = u
 
-
-def find_path(pr, node):  # generate path list based on parent points 'prev'
-    p = []
-    while node is not None:
-        p.append(node)
-        node = pr[node]
-    return p[::-1]
-
+        return dist, prev
+    
+    def find_path(self, pr, node):  # generate path list based on parent points 'prev'
+        p = []
+        while node is not None:
+            p.append(node)
+            node = pr[node]
+        return p[::-1]
 
 if __name__ == "__main__":
 
@@ -59,23 +60,31 @@ if __name__ == "__main__":
         for line in fhandle:
             edge_from, edge_to, cost = line.strip().split(" ")
             edges.append((edge_from, edge_to, int(cost)))
-    print(edges)
 
-    g = build_graph(edges)
+    graph = Graph(edges)
+
+    g = graph.build_graph()
 
     print("=== Shortest Path ===")
 
     print("--- From What City  To What City ---")
     city_from, city_to = map(str, input().strip().split())
-
-    d, prev = dijkstra(g, city_from, city_to)
-    path = find_path(prev, city_to)
-    print("{} -> {}: distance = {}, path = {}".format(city_from, city_to, d, path))
+    
+    if city_from in g and city_to in g:
+        d, prev = graph.dijkstra(city_from, city_to)
+        path = graph.find_path(prev, city_to)
+        print("{} -> {}: distance = {}, path = {}".format(city_from, city_to, d, path))
+    else:
+        print("{} or {} is not in Kazakhtan".format(city_from, city_to))
 
     print("--- From What Point To All Destinations ---")
     city_from = str(input().strip())
-    ds, prev = dijkstra(g, city_from)
+    ds, prev = graph.dijkstra(city_from)
     for k in ds:
-        path = find_path(prev, k)
+        path = graph.find_path(prev, k)
         print("{} -> {}: distance = {}, path = {}".format(city_from,k, ds[k], path))
+
+
+
+        
 
